bad722b50071a1a875bdef747b3ea0af79ecb355
Nelson <tomneo2004@gmail.com>
2022-04-03 20:10:16 +0800

    upload image

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
+from turtle import width
 import streamlit as st
 
-st.header("Streamlit is up")
+st.header("Pawpularity")
+
+with st.container():
+    uploaded_image = st.file_uploader("Upload image file", 
+                                    type=["jpeg", "jpg"], 
+                                    help="Upload an image of pet")
+
+    if uploaded_image is not None:
+        left_margin, col2, right_margin = st.columns(3)
+        with left_margin:
+            pass
+        with col2:
+            st.image(uploaded_image, caption=uploaded_image.name)
+        with right_margin:
+            pass
